a09d04f1291ea0986b0836f8d60214f2f41ace5d
JeeyoonO <eriaw29@dgu.ac.kr>
2026-05-03 21:53:07 +0900

    Feat: 입력 데이터 처리 및 결과 페이지 연동 로직 구현

--- Subject3_1/ex4.py
+++ Subject3_1/ex4.py
@@ -3,8 +3,18 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route('/')
-def main():
-    return "서버 구동" 
+def input_page():
+    return render_template('input.html')
 
-if __name__ == "__main__":
-    app.run(debug=True)
+@app.route('/result', methods=['POST', 'GET'])
+def result():
+    if request.method == 'POST':
+        result = dict()
+        result['Name'] = request.form.get('name')
+        result['StudentNumber'] = request.form.get('student_number')
+        result['Gender'] = request.form.get('gender')
+        
+        return render_template('result.html', result=result)
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
